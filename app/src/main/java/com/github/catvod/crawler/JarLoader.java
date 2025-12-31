23d30cfa40b9b67bb94d0093b69c70edd5d7594b
gungfu2020 <gungfu2020@hotmail.com>
2025-12-31 12:41:47 +0800

    update

--- app/src/main/java/com/github/catvod/crawler/JarLoader.java
+++ app/src/main/java/com/github/catvod/crawler/JarLoader.java
@@ -76,7 +76,11 @@ public class JarLoader {
         return "com.github.tvbox.osc.tk";  // ← 改成你想要的固定包名
     }
 };
+if ("main".equals(key)) {
+initMethod.invoke(null, App.getInstance());
+} else {
 initMethod.invoke(null, wrappedContext);
+}
                                     //initMethod.invoke(null, App.getInstance());
                                 } catch (Exception e) {
                                     e.printStackTrace();
@@ -185,14 +189,7 @@ initMethod.invoke(null, wrappedContext);
         try {
             Log.i("JarLoader", "echo-getSpider 加载spider: " + key);
             Spider sp = (Spider) classLoader.loadClass("com.github.catvod.spider." + clsKey).newInstance();
-	    Context wrappedContext = new ContextWrapper(App.getInstance()) {
-		    @Override
-		    public String getPackageName() {
-			    return "com.github.tvbox.osc.tk";  // ← 改成你想要的固定包名
-							       } 
-	    };
-	    sp.init(wrappedContext, ext);
-            //sp.init(App.getInstance(), ext);
+            sp.init(App.getInstance(), ext);
             if (!jar.isEmpty()) {
                 sp.homeContent(false); // 增加此行 应该可以解决部分写的有问题源的历史记录问题 但会增加这个源的首次加载时间 不需要可以已删掉
             }
